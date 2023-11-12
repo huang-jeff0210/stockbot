e557fb757aed40f25d6fb90969627853728c9e1f
huang-jeff0210 <66995440+huang-jeff0210@users.noreply.github.com>
2023-11-12 16:42:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-import re
+import re, os
 
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('7r0ld9JH1KvogGMRzjJZxSvzdSVtYu6gPF9bFMkhY/vi9YDhsBiXVagSyOOWsg2IlIV28Sc/glVyd7i+UdYFvyIgUq3L+Uz0wnyxJX8h4ziDjuu0aPckSts7Vdo7BxKZd2cNru97p0xUIHNm162OTQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('b285eec28cd07b05477bd1e879d7a23b')
+handler = WebhookHandler('391ca1cd0b1c3bdd969dd9b4c99f6429')
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -43,24 +43,23 @@ def handle_message(event):
     ### 抓到顧客的資料 ###
     profile = line_bot_api.get_profile(event.source.user_id)
     uid = profile.user_id #使用者ID
-    usespeak=str(event.message.text) #使用者講的話
+    usespeak=TextSendMessage(text=event.message.text) #使用者講的話
+    
     if re.match('新增[0-9]{4}',usespeak): # 先判斷是否是使用者要用來存股票的
-        # mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
-        line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'已經儲存成功'))
+        line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經儲存成功'))
         print(usespeak)
         return 0
 
     
     elif re.match('刪除[0-9]{4}',usespeak): # 刪除存在資料庫裡面的股票
-        # mongodb.delete_user_stock_fountion(stock=usespeak[2:])
         line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
         print(usespeak)
         return 0
 
     else:
-        message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token,message)
+        line_bot_api.reply_message(event.reply_token,usespeak)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
