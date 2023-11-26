af694a4d1cd6976177e9142a91870dc75f3ab615
huang-jeff0210 <66995440+huang-jeff0210@users.noreply.github.com>
2023-11-26 15:57:17 +0800

    Add files via upload

--- stock_srapy.py
+++ stock_srapy.py
@@ -9,7 +9,7 @@ import warnings
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-from imgurpython import Imgur
+from imgur_python import Imgur
 
 # 设置中文字体
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] # 修改中文字體
